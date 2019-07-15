c2714db6beb650651cef86930c41fa509594ae74
Paul Solt <PaulSolt@gmail.com>
2019-07-15 13:25:56 -0400

    Starting to create variables and print statements.

--- iOS6-MacOSVersions/iOS6-MacOSVersions/ViewController.m
+++ iOS6-MacOSVersions/iOS6-MacOSVersions/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LSIMacOSVersion.h"
 
 @interface ViewController ()
 
@@ -19,7 +20,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    // var version: MacOSVersion = MacOSVersion(codename: "Panther", releaseDate: "October 24, 2003")
+    
+    // 1. Allocate the memory (asking for memory to store the variable)
+    // 2. Initialize the memory
+    
+    // c-string: the c programming language
+    char *name = "Paul";    // char = character
+    // char * = character string
+    
+    
+    LSIMacOSVersion *version = [[LSIMacOSVersion alloc] initWithCodename:@"Panther"
+                                                             releaseDate:@"October 24, 2003"];
+    
+    // print()
+    // NSLog()
+    
+    NSLog(@"HI");
+    
+    // String interpolation = Swift
+//    print("My name is \(username)")
+    
+    // %@ = object (NSString, NSNumber, LSIMacOSVersion)
+    // %d = int
+    NSLog(@"My name is %@ and I'm %d years old", @"Paul", 33);
+
+    // print "Panther was released on October 24, 2003"
+    
+    // Dot syntax: version.codename
+    
 }
 
 
